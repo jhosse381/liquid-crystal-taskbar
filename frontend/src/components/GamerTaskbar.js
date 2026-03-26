061ec1d72eb6bda257b76d8c106f438048a6218f
jhosse381 <50276814+jhosse381@users.noreply.github.com>
2026-03-25 20:56:45 -0400

    Add GamerTaskbar component with glassmorphism style

---
+++ frontend/src/components/GamerTaskbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './GamerTaskbar.css';
+
+const GamerTaskbar = () => {
+  return (
+    <div className="gamer-taskbar">
+      <div className="gamer-taskbar-item">Item 1</div>
+      <div className="gamer-taskbar-item">Item 2</div>
+      <div className="gamer-taskbar-item">Item 3</div>
+    </div>
+  );
+};
+
+export default GamerTaskbar;
